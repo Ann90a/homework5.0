58b52af0b3bc43422ed63f2bdc698c4017dd3e24
Ann90a <abrosimova222@yandex.ru>
2022-11-09 13:42:32 +0300

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 19;
+        if (age >= 18) {
+            System.out.println("Поздравление пользователя с совершеннолетием");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        }
+    }
+}
